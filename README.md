441d3a7c7715408f2e9becd43c4a684553357eb1
Boulanouar <boulanouar@et.esiea.fr>
2021-03-03 21:11:38 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # maven_training
+
+![build CI](https://github.com/Souleimaneb/maven_training/actions/workflows/build.yml/badge.svg?event=push)
+[![codecov](https://codecov.io/gh/Souleimaneb/maven_training/branch/main/graph/badge.svg?token=676MFNDJGX)](https://codecov.io/gh/Souleimaneb/maven_training)
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,5 +1,3 @@
-package fr.lernejo;
-
 import fr.lernejo.Sample;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
@@ -10,11 +8,13 @@ public class SampleTest{
     public void operationMultiplyTest() throws Exception{
         Sample sample = new Sample();
         Assertions.assertEquals(sample.op(Sample.Operation.MULT, 1, 3), 3);
+        Assertions.assertNotEquals(sample.op(Sample.Operation.MULT, 3, 3), 3);
     }
 
     @Test
     public void operationAddTest() throws Exception{
         Sample sample = new Sample();
         Assertions.assertEquals(sample.op(Sample.Operation.ADD, 1, 3), 4);
+        Assertions.assertNotEquals(sample.op(Sample.Operation.ADD, 3, 3), 4);
     }
 }
